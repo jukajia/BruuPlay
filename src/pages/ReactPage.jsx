bb3729483c82bf6a8d6ea71d3a4d732f5094a80d
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-06-06 16:26:20 -0300

    Create ReactPage.jsx

---
+++ src/pages/ReactPage.jsx
@@ -0,0 +1,9 @@
+function ReactPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-primary">Página dos Memes</h1>
+    </div>
+  )
+}
+
+export default ReactPage
