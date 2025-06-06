ae735fb8537b2187bf20b66f029f73e3d1040f54
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-06-06 16:26:51 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactPage from './pages/ReactPage'
+
+function App() {
+  return (
+    <div className="bg-dark min-h-screen text-white font-sans">
+      <ReactPage />
+    </div>
+  )
+}
+
+export default App
